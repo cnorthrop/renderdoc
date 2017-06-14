bc622dfe62661d181b6542611aabf6cf03c14745
Cody Northrop <cnorthrop@google.com>
2017-06-14 14:30:47 -0600

    android: Add ability to check for RenderDoc layer and patch it in.
    After selecting an application to launch on Android, inspect it to see if
    it contains the RenderDoc layer.  If it does not, display a warning similar
    to desktop.  When clicked, offer to patch the APK and reinstall, with the
    warning that it doesn't work for all applications (or at all for GLES).
    Also provides pointers to how to package the layer yourself.
    
    The process works by using the host temp directory to pull the APK and
    modify it, using tools from the Android SDK.  If the steps fail for any
    reason, the log is populated and patching is halted.

--- renderdoc/api/replay/renderdoc_replay.h
+++ renderdoc/api/replay/renderdoc_replay.h
@@ -1455,3 +1455,12 @@ extern "C" RENDERDOC_API void RENDERDOC_CC RENDERDOC_EnumerateAndroidDevices(rdc
 
 DOCUMENT("Internal function for starting an android remote server.");
 extern "C" RENDERDOC_API void RENDERDOC_CC RENDERDOC_StartAndroidRemoteServer(const char *device);
+
+DOCUMENT("Internal function for checking for presence of RenderDoc Vulkan layer on remote device.");
+extern "C" RENDERDOC_API bool RENDERDOC_CC RENDERDOC_CheckAndroidLayer(const char *host,
+                                                                       const char *exe);
+
+DOCUMENT("Internal function that attempts to modify APK contents, adding Vulkan layer.");
+extern "C" RENDERDOC_API bool RENDERDOC_CC RENDERDOC_AddLayerToAndroidPackage(const char *host,
+                                                                              const char *exe,
+                                                                              float *progress);
